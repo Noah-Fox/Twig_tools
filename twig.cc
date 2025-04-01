93bf951d4c644420f26ce0b1eb835ba2c0ae9ffa
Noah <noah@pop-os.localdomain>
2025-04-01 16:35:50 -0400

    Read header

--- twig.cc
+++ twig.cc
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "twig.h"
 
 using namespace std;
 
 int DEBUG = 0;
+int fd = -1;
+bool flipValues = false;
 
 void cliHelp(string cmd);
 
+void readPCap();
+
+bool readPacket();
+
 int main(int argc, char *argv[]){
     string ipAddr = "";
+    string fileName;
 
     for (int i = 1; i < argc; i ++){
         if (strcmp(argv[i], "-d") == 0){
@@ -32,9 +42,102 @@ int main(int argc, char *argv[]){
     if (DEBUG){
         cout << "Beginning twig on IPv4 Address " << ipAddr << "\n";
     }
+
+    fileName = ipAddr + ".dmp";
+    fd = open(fileName.c_str(), O_RDWR);
+    if (fd == -1){
+        cerr << "Error opening " << fileName << "\n";
+        exit(0);
+    }
+    if (DEBUG){
+        cout << "Opened " << fileName << "\n";
+    }
+
+    readPCap();
 }
 
 void cliHelp(string cmd){
     cout << "Usage: " << cmd << " [-d] -i IPv4addr_masklength\n";
+    exit(0);
+}
+
+void readPCap(){
+    if (DEBUG > 1){
+        cout << "Entering Packet Capture\n";
+    }
+
+    struct pcap_file_header* pcapFileHeader = new pcap_file_header;
+    while (true){
+        int readSuccess = read(fd, pcapFileHeader, sizeof(struct pcap_file_header));
+        if (readSuccess == sizeof(struct pcap_file_header)){
+            break;
+        }
+        sleep(1);
+    }
+
+	if (pcapFileHeader->magic != PCAP_MAGIC){
+		if (ntohl(pcapFileHeader->magic) != PCAP_MAGIC){
+			fflush(stdout);
+			fprintf(stderr, "invalid magic number: 0x%08x\n", pcapFileHeader->magic);
+			exit(0);
+		}
+		pcapFileHeader->magic = ntohl(pcapFileHeader->magic);
+		pcapFileHeader->version_major = ntohs(pcapFileHeader->version_major);
+		pcapFileHeader->version_minor = ntohs(pcapFileHeader->version_minor);
+		pcapFileHeader->linktype = ntohl(pcapFileHeader->linktype);
+		pcapFileHeader->sigfigs = ntohl(pcapFileHeader->sigfigs);
+		pcapFileHeader->snaplen = ntohl(pcapFileHeader->snaplen);
+		pcapFileHeader->thiszone = ntohl(pcapFileHeader->thiszone);
+		flipValues = true;
+	}
+
+	if (pcapFileHeader->version_major != PCAP_VERSION_MAJOR || pcapFileHeader->version_minor != PCAP_VERSION_MINOR){
+		fflush(stdout);
+		fprintf(stderr, "invalid pcap version: %u.%u\n", pcapFileHeader->version_major, pcapFileHeader->version_minor);
+		exit(0);
+	}
+
+    if (DEBUG){
+        printf("PCap Header read successfully\n");
+    }
+    if (DEBUG > 1){
+        printf("\tMagic: %x\n", pcapFileHeader->magic);
+    }
+
+    while (true){
+        bool readPacketSuccess = readPacket();
+        if (!readPacketSuccess) {
+            sleep(1);
+        }
+    }
+}
+
+bool readPacket(){
+    char packetHeaderBuf[10000];
+    struct pcap_pkthdr* packetHeader;
+
+    int readSuccess = read(fd, packetHeaderBuf, sizeof(struct pcap_pkthdr));
+    if (readSuccess != sizeof(struct pcap_pkthdr)){
+        return false;
+    }
+    packetHeader = (struct pcap_pkthdr*)(packetHeaderBuf);
+
+    if (flipValues){
+        packetHeader->ts_secs = ntohl(packetHeader->ts_secs);
+        packetHeader->ts_usecs = ntohl(packetHeader->ts_usecs);
+        packetHeader->caplen = ntohl(packetHeader->caplen);
+        packetHeader->len = ntohl(packetHeader->len);
+    }
+
+    if (DEBUG){
+        printf("Packet read successful\n");
+    }
+    if (DEBUG > 1){
+        printf("\tts_secs: %x\n", packetHeader->ts_secs);
+        printf("\tts_usecs: %x\n", packetHeader->ts_usecs);
+        printf("\tcaplen: %x\n", packetHeader->caplen);
+        printf("\tlen: %x\n", packetHeader->len);
+    }
+
     exit(0);
 }
---
+++ twig.h
@@ -0,0 +1,94 @@
+#ifndef TWIG_H
+#define TWIG_H
+
+#include <netinet/in.h>
+
+typedef int32_t bpf_int32;
+typedef u_int32_t bpf_u_int32;
+
+/* every pcap file starts with this structure */
+struct pcap_file_header {
+	bpf_u_int32 magic;
+	u_short version_major;
+	u_short version_minor;
+	bpf_int32 thiszone;	/* gmt to local correction; this is always 0 */
+	bpf_u_int32 sigfigs;	/* accuracy of timestamps; this is always 0 */
+	bpf_u_int32 snaplen;	/* max length saved portion of each pkt */
+	bpf_u_int32 linktype;	/* data link type (LINKTYPE_*) */
+};
+
+#define PCAP_MAGIC         0xa1b2c3d4
+#define PCAP_VERSION_MAJOR 2
+#define PCAP_VERSION_MINOR 4
+
+const uint16_t IP_ETHER_TYPE = 0x0800;
+const uint16_t ARP_ETHER_TYPE = 0x0806;
+const uint8_t TCP_PROTO = 0x6;
+const uint8_t UDP_PROTO = 0x11;
+
+/*
+ * Generic per-packet information, as supplied by libpcap.
+ * this is the second record in the file, and every packet starts
+ * with this structure (followed by the packet date bytes)
+ */
+struct pcap_pkthdr {
+	bpf_u_int32 ts_secs;		/* time stamp */
+	bpf_u_int32 ts_usecs;	/* time stamp */
+	bpf_u_int32 caplen;	/* length of portion present */
+	bpf_u_int32 len;	/* length of this packet (off wire) */
+};
+
+struct eth_hdr {
+    uint8_t dest_addr[6];
+	uint8_t src_addr[6];
+	uint16_t ether_type;
+};
+
+struct ipv4_hdr {
+	u_char hlen:4;
+	u_char vers:4;
+	uint8_t type_serv;
+	uint16_t total_length;
+	uint16_t ident;
+	uint16_t frag;// needs parsed
+	uint8_t time;
+	uint8_t proto;
+	uint16_t check_sum;
+	uint32_t source_addr;
+	uint32_t dest_addr;
+};
+
+struct tcp_hdr {
+	uint16_t src_port;
+	uint16_t dest_port;
+	uint32_t seq_num;
+	uint32_t ack_num;
+	uint8_t hlen_res;
+	uint8_t code_bits;
+	uint16_t window;
+	uint16_t csum;
+	uint16_t urg_ptr;
+};
+
+struct udp_hdr {
+	uint16_t src_port;
+	uint16_t dest_port;
+	uint16_t length;
+	uint16_t csum;
+};
+
+struct arp_hdr {
+	uint16_t hardware_type;
+	uint16_t proto_type;
+	uint8_t hlen;
+	uint8_t plen;
+	uint16_t operation;
+	uint8_t src_haddr[6];
+	uint8_t src_iaddr[4];
+	uint8_t dest_haddr[6];
+	uint8_t dest_iaddr[4];
+};
+
+
+
+#endif
